abba5d61ca9c6066fa61df7ad243df5615097520
Molvin Steve <molvinsteven@gmail.com>
2024-12-31 18:53:13 +0300

    Another deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/Main';
 import Services from './components/Services';
 import About from './components/About';
